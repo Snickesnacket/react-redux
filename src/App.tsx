92eb9073bf7d108d057a715cda80ff7185d565bf
astapasta1337 <astapasta1337@MacBook-Pro-som-tillhor-astapasta1337.local>
2024-09-17 14:42:40 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center h-screen items-center">
+        <h1 className="text-4xl text-orange-400">Vite + React + TailwindCSS</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
